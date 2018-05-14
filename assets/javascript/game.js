139c34f1c0299faa8893a667a959a81edd894155
Andy Nguyen <anbao.ng@gmail.com>
2018-05-13 19:16:24 -0700

    handle on click event for user to choose their hero and opponent

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,38 +20,49 @@ for (let hero in heroes) {
   $('#heroes-selection').append(heroFigure);
 }
 
+
 $(".heroes-pic").on("click", function() {
+  //player choose their hero
+
+  
   player = heroes[$(this).attr("id")];
-
-
   //showing player
   $("#player img").attr('src', player.iconUrl);
   $("#player-damage").text('Damage: ' + player.damage);
   $("#player-hp").text('HP: ' + player.hp);
   $("#player").show();
+  $(this).hide();
+  $("#display-message").find("h2").text('Please Choose Your Opponent');
+  $(".heroes-pic").off("click");
 
-  //showing enemy
-  $("#enemy img").attr('src', './assets/images/qop.gif');
-  $("#enemy-counter-damage").text('Damage: ' + enemy.counterDamage);
-  $("#enemy-hp").text('HP: ' + enemy.hp);
-  $("#enemy").show();
-  
-  //attack!!!!
-  $("#attackButton").on("click", function() {
-    enemy.hp -= player.damage;
-    player.hp -= enemy.counterDamage;
-    player.damage += player.damage;
-
-    $("#player-damage").text('Damage: ' + player.damage);
-    $("#player-hp").text('HP: ' + player.hp);
-    $("#enemy-counter-damage").text('Damage: ' + enemy.counterDamage);
-    $("#enemy-hp").text('HP: ' + enemy.hp);
-
-    if(enemy.hp < 1) {
-      $("#enemy").hide();
-    }
-  })
-
+  //player choose their opponent
+  $('.heroes-pic').on('click', pickEnemy);
 })
 
 
+function pickEnemy() {
+  enemy = heroes[$(this).attr("id")];
+  //showing enemy
+  $("#enemy img").attr('src', enemy.iconUrl);
+  $("#enemy-counter-damage").text('Counter Damage: ' + enemy.counterDamage);
+  $("#enemy-hp").text('HP: ' + enemy.hp);
+  $("#enemy").show();
+  $(this).hide();
+}
+
+
+//attack!!!!
+$("#attackButton").on("click", function () {
+  enemy.hp -= player.damage;
+  player.hp -= enemy.counterDamage;
+  player.damage += player.damage;
+
+  $("#player-damage").text('Damage: ' + player.damage);
+  $("#player-hp").text('HP: ' + player.hp);
+  $("#enemy-counter-damage").text('Damage: ' + enemy.counterDamage);
+  $("#enemy-hp").text('HP: ' + enemy.hp);
+
+  if (enemy.hp < 1) {
+    $("#enemy").hide();
+  }
+})
--- assets/javascript/heroes.js
+++ assets/javascript/heroes.js
@@ -3,7 +3,7 @@ const heroes = {
     name: 'luna',
     damage: 10,
     counterDamage: 4,
-    hp: 100,
+    hp: 150,
     iconUrl: './assets/images/luna.gif',
 
   },
@@ -12,22 +12,22 @@ const heroes = {
     name: 'qop',
     damage: 7,
     counterDamage: 5,
-    hp: 100,
+    hp: 170,
     iconUrl: './assets/images/qop.gif',
   },
 
   rubick: {
     name: 'rubick',
-    damage: 7,
-    counterDamage: 5,
-    hp: 100,
+    damage: 5,
+    counterDamage: 2,
+    hp: 300,
     iconUrl: './assets/images/rubick.gif',
   },
 
   nevermore: {
     name: 'nevermore',
-    damage: 7,
-    counterDamage: 5,
+    damage: 15,
+    counterDamage: 7,
     hp: 100,
     iconUrl: './assets/images/nevermore.gif',
   },
